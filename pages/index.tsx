0939cd5999f99e045fb14ba74e584d40fe382adf
Ryoneme <75056575+Ryonem2@users.noreply.github.com>
2021-11-12 19:31:08 +0700

    gitignore test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
 import type { NextPage } from 'next'
+import Head from 'next/head'
 
 const Home: NextPage = ():any => {
   return (
     
-      <h1>Hello</h1>
+  <Head>
+    <title>Next JS login and ToDoList</title>
+  </Head>
     
   )
 }
